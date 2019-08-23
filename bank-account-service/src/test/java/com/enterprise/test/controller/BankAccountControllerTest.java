68cdd0c1369d98ad1b105b019186b472d85bc83e
Naqui <mnq.naqui@gmail.com>
2019-08-23 13:14:57 +0530

    Controller Test Cases updated

--- bank-account-service/src/test/java/com/enterprise/test/controller/BankAccountControllerTest.java
+++ bank-account-service/src/test/java/com/enterprise/test/controller/BankAccountControllerTest.java
@@ -1,25 +1,32 @@
 package com.enterprise.test.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.enterprise.BankAccountServiceApplicationTests;
+import com.enterprise.dto.BankResource;
+import com.enterprise.dto.BranchResource;
+import com.enterprise.dto.UserDetailResource;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BankAccountControllerTest extends BankAccountServiceApplicationTests {
-	
+
 	@Autowired
 	private WebApplicationContext webApplicationContext;
-	
+
 	private MockMvc mockMvc;
 	
 	@Before
@@ -27,20 +34,103 @@ public class BankAccountControllerTest extends BankAccountServiceApplicationTest
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 	
+	private static final int id=59;
 	
-	@Test
+	/*@Test
 	public void testGetBankAccounts() throws Exception {
 		mockMvc.perform(get("/bankaccount")).andExpect(status().isOk())
 				.andExpect(content().contentType("application/hal+json;charset=UTF-8"))
-				.andExpect(jsonPath("$..userId").value(3)).andExpect(jsonPath("$._links.self.href").value("http://localhost/bankaccount"));
+				.andExpect(jsonPath("$..userId").exists()).andExpect(jsonPath("$..userName").exists())
+				.andExpect(jsonPath("$..email").exists()).andExpect(jsonPath("$..mobile").exists())
+				.andExpect(jsonPath("$..stateRef").exists()).andExpect(jsonPath("$..cityRef").exists())
+				.andExpect(jsonPath("$..permanentAddress").exists()).andExpect(jsonPath("$..currentAddress").exists())
+				.andExpect(jsonPath("$..cityRef").exists()).andExpect(jsonPath("$..pincode").exists())
+				.andExpect(jsonPath("$.._links.bankaccounts").exists())
+				.andExpect(jsonPath("$._links.self.href").exists()).andExpect(jsonPath("$.._links.self").exists())
+				.andExpect(jsonPath("$._links.self.href").exists());
+
+	}
+	
+	@Test
+	public void testGetBankAccount() throws Exception {
+		mockMvc.perform(get("/bankaccount/{id}",id)).andExpect(status().isOk())
+				.andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+				.andExpect(jsonPath("$..userId").exists()).andExpect(jsonPath("$..userName").exists())
+				.andExpect(jsonPath("$..email").exists()).andExpect(jsonPath("$..mobile").exists())
+				.andExpect(jsonPath("$..stateRef").exists()).andExpect(jsonPath("$..cityRef").exists())
+				.andExpect(jsonPath("$..permanentAddress").exists()).andExpect(jsonPath("$..currentAddress").exists())
+				.andExpect(jsonPath("$..cityRef").exists()).andExpect(jsonPath("$..pincode").exists())
+				.andExpect(jsonPath("$.._links.bankaccounts").exists())
+				.andExpect(jsonPath("$._links.self.href").exists()).andExpect(jsonPath("$.._links.self").exists())
+				.andExpect(jsonPath("$._links.self.href").exists())
+				.andExpect(jsonPath("$.userId").value(id));
+
+	}
+	
+	@Test
+	public void testDeleteBankAccount() throws Exception {
+		mockMvc.perform(delete("/bankaccount/{id}", id))
+        .andExpect(status().isNoContent());
+
+	}*/
+	
+	@Test
+	public void testUpdateBankAccount() throws Exception {
+		
+		mockMvc.perform(put("/bankaccount/{id}",id)
+		.content(asJsonString(getUserResource()))
+		.contentType(MediaType.APPLICATION_JSON)
+	    .accept(MediaType.APPLICATION_JSON))
+	    .andExpect(status().isOk())
+		.andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+		.andExpect(jsonPath("$..userId").exists()).andExpect(jsonPath("$..userName").exists())
+		.andExpect(jsonPath("$..email").exists()).andExpect(jsonPath("$..mobile").exists())
+		.andExpect(jsonPath("$..stateRef").exists()).andExpect(jsonPath("$..cityRef").exists())
+		.andExpect(jsonPath("$..permanentAddress").exists()).andExpect(jsonPath("$..currentAddress").exists())
+		.andExpect(jsonPath("$..cityRef").exists()).andExpect(jsonPath("$..pincode").exists())
+		.andExpect(jsonPath("$.._links.bankaccounts").exists())
+		.andExpect(jsonPath("$._links.self.href").exists()).andExpect(jsonPath("$.._links.self").exists())
+		.andExpect(jsonPath("$._links.self.href").exists())
+		.andExpect(jsonPath("$.userId").value(id));
 
 	}
 	
 	public static String asJsonString(final Object obj) {
-	    try {
-	        return new ObjectMapper().writeValueAsString(obj);
-	    } catch (Exception e) {
-	        throw new RuntimeException(e);
-	    }
+		try {
+			String json = new ObjectMapper().writeValueAsString(obj);
+			return json;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
+	
+	public static UserDetailResource getUserResource() {
+		UserDetailResource resource=new UserDetailResource();
+		resource.setUserName("Naqui Qureshi");
+		resource.setPermanentAddress("Chhindwara");
+		resource.setEmail("naqui.qureshi@evry.com");
+		//resource.setMobile("900842788");
+		//resource.setCurrentAddress("HSR Bangalore");
+		resource.setCityRef(1);
+		resource.setStateRef(2);
+		resource.setPincode("560101");
+		
+		ArrayList<BankResource> bankList=new ArrayList<>();
+		ArrayList<BranchResource> branchList=new ArrayList<>();
+		
+		BankResource bank=new BankResource();
+		
+		BranchResource branchResource = new BranchResource();
+		branchResource.setBranchId(1);
+		bank.setBankId(1);
+		bank.setBranches(branchList);
+		
+		branchList.add(branchResource);
+		
+		resource.setBanks(bankList);
+		
+		return resource;
+	}
+	
+	
 }
